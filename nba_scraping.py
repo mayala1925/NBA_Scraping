cf3e86d4b528042eca89714d0db94413d39112d5
Matt Ayala <matthewayala@Matts-MacBook-Pro.local>
2021-11-03 10:43:09 -0600

    created function that will pull all given months stats

--- nba_scraping.py
+++ nba_scraping.py
@@ -6,68 +6,80 @@ Created on Wed Dec 23 11:00:15 2020
 @author: matthewayala
 """
 
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
 
 # NBA Season to analyze
-year = 2021
+year = 2022 # Season
+months = ['october','november'] # Months of the season
 
-# URL Page to be extracted
-url = "https://www.basketball-reference.com/leagues/NBA_{}_games.html".format(year)
+def get_stats(year,month):
+    # Creating an empty dataframe to append stats too.
+    cols = ['date', 'start', 'away', 'away_pts', 'home', 'home_pts', 'attend']
+    final_df = pd.DataFrame(columns = cols)
 
-# HTML from the given URL
-html = urlopen(url)
+    for i in month:
+        url = f"https://www.basketball-reference.com/leagues/NBA_{year}_games-{i}.html"
 
-soup = BeautifulSoup(html, features = 'lxml')
+        # HTML from the given URL
+        html = urlopen(url)
+        soup = BeautifulSoup(html, features = 'lxml')
+        soup.findAll("tr", limit=2)
 
-# Using FindAll to get column headers
-soup.findAll("tr",limit = 2)
+        # Using getText to extract the text needed
+        headers = [th.getText() for th in soup.findAll("tr", limit=2)[0].findAll('th')]
 
-# Using getText to extract the text needed
-headers = [th.getText() for th in soup.findAll("tr",limit = 2)[0].findAll('th')]
+        headers = headers[1:]
 
-headers = headers[1:]
+        # Extracting the data for the rows including dates of games.
+        rows = soup.findAll("tr")[1:]
+
+        game_stats = [[td.getText() for td in rows[i].findAll('td')]
+                      for i in range(len(rows))]
+
+        game_dates = [[th.getText() for th in rows[i].findAll('th')]
+                      for i in range(len(rows))]
+
+        # Putting data and column headers into a dataframe and dropping Boxscore,Notes,OT Columns
+        stats_df = pd.DataFrame(game_stats, columns=headers)
+
+        # Reorganizing columns, putting date as the first column.
+        stats_df.insert(0, 'Date', game_dates)
+        stats_df.drop(stats_df.columns[[6, 9]], axis=1, inplace=True)
+
+        # Renaming columns for accessibility and readability
+        stats_df.columns = cols
+
+        # Dropping commas from dataframe so columns can be read as float64
+        stats_df = stats_df.replace(',', '', regex=True)
+
+        # Converting columns into the correct data types.
+        stats_df['date'] = pd.to_datetime(stats_df.date.apply(lambda x: x[0]))
+        stats_df.iloc[:, 3] = pd.to_numeric(stats_df.iloc[:, 3])
+        stats_df.iloc[:, 5] = pd.to_numeric(stats_df.iloc[:, 5])
+        stats_df.iloc[:, 6] = pd.to_numeric(stats_df.iloc[:, 6])
+
+        # Adding a winner column
+        stats_df['winner'] = np.where((stats_df['away_pts'] > stats_df['home_pts']), 'away', 'home')
+
+        # Append months stats to the previous stats.
+        final_df = final_df.append(stats_df)
+
+    return final_df
+
+nba_stats = get_stats(2022,months)
 
 
-# Extracting the data for the rows including dates of games.
-rows = soup.findAll("tr")[1:]
 
-game_stats =[[td.getText() for td in rows[i].findAll('td')]
-             for i in range(len(rows))]
 
-game_dates = [[th.getText() for th in rows[i].findAll('th')]
-              for i in range(len(rows))]
+# Option to write to an excel file.
+# with pd.ExcelWriter('nba_scraping.xlsx') as writer:
+#     nba_stats.to_excel(writer, sheet_name='Main NBA Data')
 
-# Putting data and column headers into a dataframe and dropping Boxscore,Notes,OT Columns
-stats = pd.DataFrame(game_stats,columns = headers)
 
-# Reorganizng columns, putting date as the first column.
-stats.insert(0,'Date',game_dates)
-stats.drop(stats.columns[[6,9]], axis = 1, inplace = True)
-
-# Renaming columns for accessibility and readability
-stats.columns = ['Date','Start(ET)','Away','Away PTS','Home','Home PTS','Attend.']
-
-# Dropping commas from dataframe so columns can be read as float64
-stats = stats.replace(',','', regex=True)
-
-# Converting columns into the correct data types.
-stats['Date'] = pd.to_datetime(stats.Date.apply(lambda x: x[0]))
-stats.iloc[:,3] = pd.to_numeric(stats.iloc[:,3])
-stats.iloc[:,5] = pd.to_numeric(stats.iloc[:,5])
-stats.iloc[:,6] = pd.to_numeric(stats.iloc[:,6])
-
-# For some reason applying pd.to_numeric to all the columns at once was changing values
-#stats.iloc[:,[3,5,6]] = stats.iloc[:,[3,5,6]].apply(pd.to_numeric)
-
-# Adding a column for the winner of the game
-stats['Winner'] = np.where(stats['Home PTS'] > stats['Away PTS'],stats['Home'],stats['Away'])
-
-with pd.ExcelWriter('nba_scraping.xlsx') as writer:
-    stats.to_excel(writer, sheet_name='Main NBA Data')
-   # nba_scoring_averages.to_excel(writer, sheet_name='HOME-ROAD Splits')
 
 
 
