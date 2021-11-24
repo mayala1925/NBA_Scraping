068f0b2dd5d352914420463119b546c86abfd2cb
Matthew Ayala <mayala1925@gmail.com>
2021-11-24 11:05:46 -0700

    Adding adjusted ratings to schedule data (more features)

--- new_nba.py
+++ new_nba.py
@@ -14,7 +14,7 @@ ratings_dict = {}
 today = date.today()
 
 sdate = date(2021, 11, 15)   # start date
-edate = today   # end date
+edate = today   # end date (today)
 
 delta = edate - sdate       # as timedelta
 
@@ -58,9 +58,29 @@ for key,value in ratings_dict.items():
     schedule.loc[schedule['date'] == key, 'a_NRtg'] = schedule['away'].map(make_stat_dictionary(value,'NRtg'))
     schedule.loc[schedule['date'] == key, 'h_NRtg'] = schedule['home'].map(make_stat_dictionary(value,'NRtg'))
 
+    # Margin of Victory/Adjusted for strength of opponent (MOV/A)
+    schedule.loc[schedule['date'] == key, 'a_mov/a'] = schedule['away'].map(make_stat_dictionary(value,'MOV/A'))
+    schedule.loc[schedule['date'] == key, 'h_mov/a'] = schedule['home'].map(make_stat_dictionary(value,'MOV/A'))
+
+    # Offensive Rating/ Adjusted for opponent defense (ORtg/A)
+    schedule.loc[schedule['date'] == key, 'a_ORtg/a'] = schedule['away'].map(make_stat_dictionary(value,'ORtg/A'))
+    schedule.loc[schedule['date'] == key, 'h_ORtg/a'] = schedule['home'].map(make_stat_dictionary(value,'ORtg/A'))
+
+    # Defensive Rating/ Adjusted for opponent offense (DRtg/A)
+    schedule.loc[schedule['date'] == key, 'a_DRtg/a'] = schedule['away'].map(make_stat_dictionary(value,'DRtg/A'))
+    schedule.loc[schedule['date'] == key, 'h_DRtg/a'] = schedule['home'].map(make_stat_dictionary(value,'DRtg/A'))
+
+    # Net Rating/ Adjusted for opponent strength (NRtg/A)
+    schedule.loc[schedule['date'] == key, 'a_NRtg/a'] = schedule['away'].map(make_stat_dictionary(value,'NRtg/A'))
+    schedule.loc[schedule['date'] == key, 'h_NRtg/a'] = schedule['home'].map(make_stat_dictionary(value,'NRtg/A'))
+
+
+
 # Cleaning up the columns and rearranging to how I want it.
 schedule = schedule.drop(['start','attend'],axis = 1)
-schedule_cols = ['date', 'away', 'away_pts', 'home', 'home_pts', 'a_mov', 'h_mov', 'a_ORtg', 'h_ORtg', 'a_DRtg', 'h_DRtg', 'a_NRtg', 'h_NRtg', 'winner']
+schedule_cols = ['date', 'away', 'away_pts', 'home', 'home_pts', 'a_mov', 'h_mov',
+                 'a_ORtg', 'h_ORtg', 'a_DRtg', 'h_DRtg', 'a_NRtg', 'h_NRtg',
+                 'a_mov/a','h_mov/a','a_ORtg/a','h_ORtg/a','a_DRtg/a','h_DRtg/a','a_NRtg/a','h_NRtg/a','winner',]
 schedule = schedule[schedule_cols]
 
 # Dropping rows for data that we don't have yet. (today + 1 day)
@@ -79,7 +99,7 @@ def take_out_list(column):
     return replace_col
 
 # looping through each column that needs to be converted from a list
-for i in schedule.columns[5:13]:
+for i in schedule.columns[5:21]:
    schedule[i] = take_out_list(schedule[i])
 
 schedule.to_csv(f'nba_scrapping_data/schedule_ratings.csv',index = False)
