25fac4b32fd036756014bb5db28c656c84a69eb1
mayala1925 <mayala1925@gmail.com>
2021-12-11 12:45:09 -0700

    Added ratings for up to 2021/12/11 and calculating mean squared error and R2 of each model, very bad...lol

--- nba_classifier.py
+++ nba_classifier.py
@@ -24,19 +24,17 @@ data = pd.read_csv('nba_scrapping_data/schedule_ratings.csv')
 # Drop rows with null values, these are the rows that were pulled that the games haven't been played yet...
 classifier_data = data.dropna()
 
+# Creating a target label for regression models.
+classifier_data['a_wonby'] = classifier_data['away_pts'] - classifier_data['home_pts']
+
 # Taking the rows that were dropped to make predictions.
 pred_rows = data[data['date'] >= str(today)]
 pred_features = pred_rows[pred_rows.columns[5:21]]
 
-# Encoding labels to 1's and 0's (Not sure if this makes any difference?)
-# label_codes = {'home':1, 'away':0}
-#
-# classifier_data['winner'] = classifier_data['winner'].map(label_codes)
-
 # Selecting the columns with the stats we want to use as features
 features = classifier_data[classifier_data.columns[5:21]]
-target = classifier_data[classifier_data.columns[-1]]
-
+class_target = classifier_data[classifier_data.columns[-2]] # This target is the discrete variable of home or away team winning.
+reg_target = classifier_data[classifier_data.columns[-1]] # This target variable is how much the away team won by.
 
 N_SPLITS = 5
 
@@ -69,7 +67,7 @@ pipe_dict = {0: 'Logistic Regression',
              3: 'Linear Discriminant Analysis',
              4: 'Support Vector Machine'}
 
-X_train, X_test, y_train, y_test = train_test_split(features, classifier_data['winner'], test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(features, class_target, test_size=0.2, random_state=0)
 
 predictions = []
 for pipe in pipelines:
@@ -87,9 +85,6 @@ for i in range(len(predictions[0])):
         pred_list.append(pred[i])
     home_count = pred_list.count('home')
     away_count = pred_list.count('away')
-    print(pred_list)
-    print(home_count)
-    print(away_count)
     if home_count > away_count:
         final_predictions.append('Home')
     elif home_count == away_count:
@@ -142,12 +137,71 @@ kf = KFold(n_splits=N_SPLITS)
 
 #%%
 # Trying Gradient Boosting Classifier
+scaler = StandardScaler()
+scaled_features = scaler.fit_transform(features)
 
 gbm = GradientBoostingClassifier(random_state=1, n_estimators=120, learning_rate= 0.1, max_depth=3)
 cv = StratifiedKFold(n_splits=5,random_state=1, shuffle=True)
 
-n_scores = cross_val_score(gbm, features, y=target, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
+n_scores = cross_val_score(gbm, scaled_features, y=class_target, scoring='accuracy', cv=cv, n_jobs=-1, error_score='raise')
 
 
 
 print(f'Gradient Boosting Accuracy: {np.mean(n_scores)}, {np.std(n_scores)}')
+
+#%%
+# Creating multiple regression models.
+
+print('NOW FOR THE REGRESSION MODELS...')
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.svm import SVR
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+
+# Scaling data first
+
+scaled_features = scaler.fit_transform(features)
+
+X_train_reg, X_test_reg, y_train_reg, y_test_reg = train_test_split(features, reg_target, test_size=0.2, random_state=0)
+
+lm_model = LinearRegression()
+
+lm_model.fit(X_train_reg,y_train_reg)
+
+# print(f'Multiple Linear Regression Score: {lm_model.score(X_test_reg,y_test_reg)}')
+
+# Creating pipelines
+pipeline_lm = Pipeline([('scalar1', StandardScaler()),
+                        ('lm_classifier', LinearRegression())])
+
+pipeline_r = Pipeline([('scalar2', StandardScaler()),
+                        ('r_classifier', Ridge())])
+
+pipeline_dtr = Pipeline([('scalar3', StandardScaler()),
+                        ('dtr_classifier', DecisionTreeRegressor(random_state=0))])
+
+pipeline_svr = Pipeline([('scalar3', StandardScaler()),
+                        ('svr_classifier', SVR())])
+
+pipelines_reg = [pipeline_lm, pipeline_r, pipeline_dtr, pipeline_svr]
+
+# best_accuracy = 0.0
+# best_classifier = 0
+# best_pipeline = ""
+
+pipe_dict_reg = {0: 'Linear Regression',
+             1: 'Ridge',
+             2: 'Decision Tree Regressor',
+             3: 'Support Vector Regressor'
+                 }
+
+for pipeline in pipelines_reg:
+    pipeline.fit(X_train_reg, y_train_reg)
+
+for i, model in enumerate(pipelines_reg):
+    reg_pred = model.predict(X_test_reg)
+    mse_score = mean_squared_error(y_test_reg,reg_pred)
+    r_score = r2_score(y_test_reg,reg_pred)
+    print(f'{pipe_dict_reg[i]}: Mean Squared Error: {mse_score},R2:{r_score}')
